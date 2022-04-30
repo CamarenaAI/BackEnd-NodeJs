17bf237aee737320eb76c0e5bb10bb200d27994c
Alonso Ismael Camarena <71803835+IsmaelCamna@users.noreply.github.com>
2022-04-29 22:07:49 -0500

    Update main.js

--- weekly_mission_2/example1/main.js
+++ weekly_mission_2/example1/main.js
@@ -21,3 +21,20 @@ const twitter = {
    console.log(twitter.url)
    console.log(twitter.getBio())
    console.log(twitter.getGeneralInfo())
+
+const facebook = {
+    username: "Ismael C. Zamora",
+    followers: "35",
+    friends: "288",
+    country: "Mexico",
+    url: "https://www.facebook.com/ismael.cz128/",
+    cellphone_number: "33 3473 8149",
+
+    getBio: function(){
+        return `I'm from: ${this.country} Number of friends: ${this.friends} Number of followers: ${this.followers} Cellphone: 33 3473 8149`
+    }
+   }
+   
+   console.log("Nombre de usuario: " + facebook.username)
+   console.log(facebook.getBio())
+   console.log(facebook.url)
