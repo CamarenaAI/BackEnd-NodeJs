b09f42efc36bd536035a24f774490dc8db1b22b3
Alonso Ismael Camarena <71803835+IsmaelCamna@users.noreply.github.com>
2022-04-29 22:13:45 -0500

    Update main.js

--- weekly_mission_2/example1/main.js
+++ weekly_mission_2/example1/main.js
@@ -38,3 +38,19 @@ const facebook = {
    console.log("Nombre de usuario: " + facebook.username)
    console.log(facebook.getBio())
    console.log(facebook.url)
+
+const uber = {
+    fisrt_name: "Ismael ",
+    last_name: "CZ",
+    cellphone_number: "33 3473 8149",
+    payment: "Credit/Debit Card, PayPal, Google Play, Cash",
+    email: "alonsocz200@hotmail.com",
+    
+    getGeneralInfo: function(){
+      return `User: ${this.fisrt_name} ${this.last_name}, Email: ${this.email}`
+    }
+   }
+   
+   console.log(uber.getGeneralInfo())   
+   console.log(uber.cellphone_number)
+   console.log(uber.payment)
