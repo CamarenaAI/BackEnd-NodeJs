0c76273e5ae780c1ad50a78c7ec6b880791a3ae9
IsmaelCamna <alonsocz200@hotmail.com>
2022-04-29 22:12:04 -0500

    Updated

---
+++ weekly_mission_2/example1/main.js
@@ -0,0 +1,56 @@
+const twitter = {
+    user: "Ismael",
+    username: "CamarenaAI",
+    url: "https://twitter.com/CamarenaAI",
+    country: "Mex.",
+    carrer: "Mechatronic Engineer ⚙️.",
+    hobbies: "Programmer Python/C/C++ 💻, Azure Tech ☁️.",
+    innova: "Viajero at Innovación Virtual ☁️, Explorer at LaunchX 🚀.",
+    hashtag: " #InnovacciónVirtual, #DataRangers, #MakingAI", 
+    
+    getBio: function(){
+        return `I'm from: ${this.country} Carrer: ${this.carrer} My Hobbies: ${this.hobbies} I am: ${this.innova}`
+    },
+    getGeneralInfo: function(){
+      return `User: ${this.username} this profile is from ${this.user}`
+    }
+   }
+   
+   console.log("Nombre del usuario: " + twitter.user)
+   console.log("hashtags: " + twitter.hashtag)
+   console.log(twitter.url)
+   console.log(twitter.getBio())
+   console.log(twitter.getGeneralInfo())
+
+const facebook = {
+    username: "Ismael C. Zamora",
+    followers: "35",
+    friends: "288",
+    country: "Mexico",
+    url: "https://www.facebook.com/ismael.cz128/",
+    cellphone_number: "33 3473 8149",
+
+    getBio: function(){
+        return `I'm from: ${this.country} Number of friends: ${this.friends} Number of followers: ${this.followers} Cellphone: 33 3473 8149`
+    }
+   }
+   
+   console.log("Nombre de usuario: " + facebook.username)
+   console.log(facebook.getBio())
+   console.log(facebook.url)
+
+const uber = {
+    fisrt_name: "Ismael ",
+    last_name: "CZ",
+    cellphone_number: "33 3473 8149",
+    payment: "Credit/Debit Card, PayPal, Google Play, Cash",
+    email: "alonsocz200@hotmail.com",
+    
+    getGeneralInfo: function(){
+      return `User: ${this.fisrt_name} ${this.last_name}, Email: ${this.email}`
+    }
+   }
+   
+   console.log(uber.getGeneralInfo())   
+   console.log(uber.cellphone_number)
+   console.log(uber.payment)
