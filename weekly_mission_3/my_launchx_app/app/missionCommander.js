19585c584bc49d25d49b8e45d8e05d335667b4da
IsmaelCamna <alonsocz200@hotmail.com>
2022-05-03 23:58:23 -0500

    updated

---
+++ weekly_mission_3/my_launchx_app/app/missionCommander.js
@@ -0,0 +1,10 @@
+class MissionCommander {
+  constructor (name, missionCommander, innova) {
+    this.name = name
+    this.missionCommander = missionCommander
+    this.innova = innova 
+  }
+}
+
+// Esta línea nos permite exportar nuestra clase
+module.exports = MissionCommander
---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,5 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo", "MC BackEnd", "LaunchX")
+console.log(carlo.name)
+console.log(carlo.missionCommander)
+console.log(carlo.innova)
